21d586cb03316d61ea0b860b8d989585e247c806
Brett Salyer <brettsalyer@gmail.com>
2020-04-17 10:40:16 -0400

    Added main game files to which we will boot on startup

---
+++ snake/bullworm.c
@@ -0,0 +1,9 @@
+#include "src/button.h"
+#include "bullworm.h"
+
+int kernelmain(int argc, char **argv)
+{
+	
+
+	return 0;
+}
---
+++ snake/src/compile.sh
@@ -0,0 +1,2 @@
+gcc -fno-PIC -g -c -m16 -ffreestanding -Os -Wall -fomit-frame-pointer button.c -o libbutton.o
+
