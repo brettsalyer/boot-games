102f825cbd43d9faa9b232fc0b4903a221519fc7
Brett Salyer <brettsalyer@gmail.com>
2020-04-17 11:51:19 -0400

    began work on creating a menu for the bullworm game

--- snake/bullworm.c
+++ snake/bullworm.c
@@ -3,7 +3,27 @@
 
 int kernelmain(int argc, char **argv)
 {
-	
+	// Create some menu buttons
+	struct Button play_button;
+	struct Button quit_button;
+	struct Button leaderboard_button;
+
+	// Initialize Buttons
+	button_init(&play_button, "Play", 50, 50, 0x00);
+	button_init(&quit_button, "Quit", 150, 40, 0x00);
+	button_init(&leaderboard_button, "Leaderboard", 150, 60, 0x00);
+
+	button_set_selected(&play_button, 1);
+
+
+	// Set video mode
+	set_video_mode(0x0D);
+
+	// Clear the screen
+	//clear_screen();
+
+	// Show the button
+	show_button(&play_button);
 
 	return 0;
 }
