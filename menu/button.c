35e23ec27d28e76eec24849af450a65957430798
Brett Salyer <brettsalyer@gmail.com>
2020-04-20 12:54:02 -0400

    Resolved relative header addressing problem

--- menu/button.c
+++ menu/button.c
@@ -1,5 +1,5 @@
 #include "button.h"
-#include "../../graphics/graphics.h"
+#include "../graphics/graphics.h"
 
 
 void button_init(struct Button * button, char * text, int x, int y, void (*handler)()) {
